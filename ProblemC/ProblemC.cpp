88c830548ffcddc8806a6eb16843beba050dd686
Denis Czech <117384166+CzechU99@users.noreply.github.com>
2024-02-29 08:49:02 +0100

    Add files via upload

---
+++ ProblemC/ProblemC.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ 
+	int t, a, e, suma = 0, c, sum, k, m, test;
+
+	cin >> t;
+
+	while (t--) {
+
+		suma = 0;
+		test = 0;
+
+		cin >> a;
+
+		int *ar = new int[a];
+
+		for (int i = 0; i < a; i++) {
+			cin >> e;
+			suma += e;
+			ar[i] = e;
+		}
+
+		c = suma;
+
+		c /= 2;
+
+		while (true) {
+
+			test = 0;
+
+			for (int i = 0; i < a; i++) {
+				if (ar[i] == c) {
+					test = c;
+					break;
+				}
+			}
+			if (test == c) {
+				break;
+			}
+			for (int i = 0; i < a; i++) {
+				m = i + 1;
+				test = 0; 
+				for (int q = i; q < a; q++) {
+					k = i;
+					sum = 0;
+					test = 0;
+					while (k != m) {
+						sum += ar[k];
+						k++;
+					}
+					for (int x = m; x < a; x++) {
+						test = sum;
+						test += ar[x];
+						if (test == c) {
+							break;
+						}
+					}
+
+					m++;
+
+					if (test == c) {
+						break;
+					}
+
+				}
+
+				if (test == c) {
+					break;
+				}
+
+			}
+
+			if (c == test || c == 0) {
+				break;
+			}
+
+			c--;
+
+		}
+
+		cout << suma - (2 * test) << '\n';
+
+	}
+
+}
